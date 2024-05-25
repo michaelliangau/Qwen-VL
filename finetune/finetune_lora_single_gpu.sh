4f46cdd88303cb873e9db18ac3541530c12a0710
Michael Liang <michaelliang15@gmail.com>
2024-05-24 20:39:14 -0400

    Update finetune_lora_single_gpu.sh

--- finetune/finetune_lora_single_gpu.sh
+++ finetune/finetune_lora_single_gpu.sh
@@ -6,22 +6,24 @@ DIR=`pwd`
 MODEL="Qwen/Qwen-VL-Chat" #"Qwen/Qwen-VL-Chat"/"Qwen/Qwen-VL" # Set the path if you do not want to load from huggingface directly
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
-DATA="path_to_data"
+DATA="/home/michael/ai/projects/computer_agent/data/2_dot_qwen.json"
+EVAL_DATA="/home/michael/ai/projects/computer_agent/data/2_dot_qwen_eval.json"
 
 export CUDA_VISIBLE_DEVICES=0
 
 python finetune.py \
     --model_name_or_path $MODEL \
     --data_path $DATA \
+    --eval_data_path $EVAL_DATA \
     --bf16 True \
     --fix_vit True \
     --output_dir output_qwen \
     --num_train_epochs 5 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 8 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "epoch" \
+    --save_strategy "epoch" \
     --save_steps 1000 \
     --save_total_limit 10 \
     --learning_rate 1e-5 \
@@ -29,9 +31,10 @@ python finetune.py \
     --adam_beta2 0.95 \
     --warmup_ratio 0.01 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --report_to "none" \
+    --logging_steps 10 \
+    --report_to "wandb" \
     --model_max_length 2048 \
     --lazy_preprocess True \
     --gradient_checkpointing \
+    --dataloader_num_workers 12 \
     --use_lora
